aebc58912864df41249638f851c2122f7239a85b
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-01-31 13:49:17 +0700

    Completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  const clearText = text.toLowerCase()
+                        .split('')
+                        .filter((item)=>{
+                          return alphanumerical.includes(item)
+                        })
+                        .reduce((prev,item)=>{
+                          return prev + item
+                        },'')
+  
+  const reversedText = clearText.split('').reverse().join('')
+
+  return clearText === reversedText ? true : false
 };
 
 // Do not edit below this line
