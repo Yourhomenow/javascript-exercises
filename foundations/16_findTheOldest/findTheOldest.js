2fced730a8f9ca71675ecc77260124ff6b96e109
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-01-31 18:40:49 +0700

    Completed

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+function getAge(birth,death){
+  let yearNow = (new Date).getFullYear()
+  if (death !== undefined){
+    return death - birth
+  } else {
+    return yearNow - birth
+  }
+}
 
+
+const findTheOldest = function(arr) {
+
+  let sorted = arr.reduce((oldest,current)=>{
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath)
+
+    return oldestAge > currentAge ? oldest : current
+  })
+
+  return sorted
 };
 
 // Do not edit below this line
