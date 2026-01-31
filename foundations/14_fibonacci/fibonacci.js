4cea29825ab76153b807cb2737ecc178beac506d
Yourhomenow <123375532+Yourhomenow@users.noreply.github.com>
2026-01-31 15:09:01 +0700

    Completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let fiboArr = [0,1,1]
+  // if ((num == 1) || (num == 2)) return 1
+
+  if (num < 0){
+    return "OOPS"
+  }
+
+  let i = 3
+  while (i <= num){
+    let cur = fiboArr[i-2] + fiboArr[i-1]
+    fiboArr.push(cur)
+    i++
+  }
+  return fiboArr[num]
 
 };
 
